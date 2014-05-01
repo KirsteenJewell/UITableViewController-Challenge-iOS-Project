6d2a9911255e7cf610506d0ad900108e5cb3212a
Kirsteen Jewell <kirsteen.jewell@gmail.com>
2014-05-01 17:01:43 +0100

    Initial Commit

---
+++ UITableViewController Challenge Solution/KJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KJAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Kirsteen Jewell on 01/05/2014.
+//  Copyright (c) 2014 limitless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
