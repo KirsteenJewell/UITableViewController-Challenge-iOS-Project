891617ee7268b5ff0d9c6e133507d521b74d4c98
Kirsteen Jewell <kirsteen.jewell@gmail.com>
2014-05-02 10:43:37 +0100

    Added project work for the challenge

---
+++ UITableViewController Challenge Solution/KJTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  KJTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Kirsteen Jewell on 01/05/2014.
+//  Copyright (c) 2014 limitless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJTableViewController : UITableViewController
+
+@end
